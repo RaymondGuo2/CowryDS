6241470703193fdaaf8da00f56b0fde663222e91
rayqg22 <72386070+rayqg22@users.noreply.github.com>
2025-06-29 20:05:53 +0100

    Text classification

--- scripts/task2.py
+++ scripts/task2.py
@@ -3,6 +3,9 @@ from nlp import obtain_corpus, normalise_corpus, build_feature_matrix, get_topic
 import pandas as pd
 from sklearn.decomposition import LatentDirichletAllocation
 from sentiment import analyse_sentiment_textblob
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 
 df_control = pd.read_excel('../data_source/CDS_25_Task2.xlsx', 'C Control')
 general_corpus = obtain_corpus(df_control)
@@ -33,3 +36,16 @@ sentiment_df = pd.DataFrame({
 })
 print(sentiment_df.head())
 sentiment_df.to_csv('../data_source/sentiment_analysis_results.csv', index=False)
+
+# Train classification model
+topic_distributions = lda.transform(tfidf_matrix)
+print(topic_distributions)
+
+binary_labels = [1 if label == 'positive' else 0 for label in sentiment_labels]
+X_train, X_test, y_train, y_test = train_test_split(topic_distributions, binary_labels, test_size=0.2, random_state=42)
+
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+print(classification_report(y_test, y_pred))
